42d574331fc327904c865946ff7176b4d7aebaee
nfalgo <52975795+nfalgo@users.noreply.github.com>
2019-07-17 06:07:06 +0900

    Create 112.cpp
    211 Byte		1 ms	256 KB

---
+++ A/112.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n,a,b;
+  cin >> n;
+  if (n == 1) {
+    cout << "Hello World";
+  } else {
+    cin >> a >> b;
+    cout << a+b;
+  }
+
+
+  return 0;
+}
