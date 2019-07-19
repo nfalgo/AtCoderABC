95e128ef6d665121a4ba30d26dfb4c4e8c6d7da0
nfalgo <52975795+nfalgo@users.noreply.github.com>
2019-07-20 06:42:30 +0900

    Create 104.cpp
    201 Byte		1 ms	256 KB

---
+++ A/104.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int R;
+  cin >> R;
+
+  if (R < 1200) cout << "ABC";
+  else if (R < 2800) cout << "ARC";
+  else cout << "AGC";
+  
+  return 0;
+}
